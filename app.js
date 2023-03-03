24524f201afb28fa1b82db44ce905a4f0106f84a
unknown <yourajsharma2003@gmail.com>
2023-03-03 23:36:09 +0530

    Successfully incorporated MongoDB database to this blog website.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-//jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -14,60 +14,77 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-var posts = [];
+mongoose.set('strictQuery', false);
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
 
 
-app.get("/", function(req, res){
-  res.render("home", {homeStartingContent: homeStartingContent, posts: posts});
-   
-  
+app.get("/", function (req, res) {
+  // res.render("home", {
+  //   startingContent: homeStartingContent,
+  //   posts: posts
+  //   });
+
+  Post.find().then(posts => {
+    res.render("home", {
+      homeStartingContent: homeStartingContent,
+      posts: posts
+    });
+  })
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent });
+
+
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent });
-  
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+
 });
 
-app.get("/compose", function(req, res){
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function (req,res){
+app.post("/compose", function (req, res) {
 
-   const post = {
-    title : req.body.title,
-    content :  req.body.content
-  };
-  posts.push(post);
-
-  res.redirect("/");
-
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(() => {
+    console.log('Post added to DB.');
+    res.redirect('/');
+  })
+    .catch(err => {
+      // res.status(400).send("Unable to save post to database.");
+      console.log(err + "Unable to save post to database.");
+    });
 });
 
 
-app.get("/posts/:postName", function(req,res){
-    var pn = req.params.postName;
-    posts.forEach(post => {
-     var pt =  post.title; 
-     if (_.lowerCase(pt)==_.lowerCase(pn)) {
-      res.render("post", { pt: post.title, pc: post.content });}
-});
-});
- 
+app.get("/posts/:postId", function(req, res) {
+  const requestedPostId = req.params.postId;
+  Post.findOne({_id: requestedPostId}).then(post =>{
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+   });
+  });
 
-
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
